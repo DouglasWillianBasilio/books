457736dff8c2b99532034b1b2e5e0843352a68eb
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-05-20 16:58:46 -0300

    configurando o styled components e retirando o css

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,15 +1,33 @@
-import './estilo.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+`
+
+const LogoImage = styled.img`
+  margin-right: 10;
+`
+
+const LogoP = styled.p`
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding-top: 25px;
+  margin-left: 15px;
+`
+
+
 
 export default function Logo() {
   return (
-    <div className='logo'>
-      <img
+    <LogoContainer>
+      <LogoImage
         src={logo}
         alt='logo'
-        className='logo-img'>
-      </img>
-      <p className='logo-p'><strong>Books</strong></p>
-    </div>
+      />
+      <LogoP><strong>Books</strong></LogoP>
+    </LogoContainer>
   )
 }
--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,4 +1,4 @@
-import './estilos.css'
+import styled from 'styled-components'
 
 const textoOpcoes = [
     'CATEGORIAS',
@@ -6,12 +6,29 @@ const textoOpcoes = [
     'MINHA ESTANTE'
 ]
 
+const Opcoes = styled.ul `
+    gap: 15px;
+    display: flex;
+`
+
+const Opcao = styled.li `
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-height: 120px;
+`
+
 export default function OpcoesHeader() {
     return (
-        <ul className='opcoes'>
+        <Opcoes>
             {textoOpcoes.map((texto) => (
-                <li className='opcao'><p>{texto}</p></li>
+                <Opcao><p>{texto}</p></Opcao>
             ))}
-        </ul>
+        </Opcoes>
     )
 }
